e9613f5d9dec24d5126578d6f138d09d407c8927
Nathan Craddock <nzcraddock@gmail.com>
2021-09-08 19:52:37 -0600

    Bump version to 5.11.3

--- zygrader/config/shared.py
+++ zygrader/config/shared.py
@@ -8,7 +8,7 @@ from . import preferences
 
 class SharedData:
     # Zygrader version
-    VERSION = LooseVersion("5.11.2")
+    VERSION = LooseVersion("5.11.3")
 
     # Current class code (shared)
     # Can be overridden on a user level
